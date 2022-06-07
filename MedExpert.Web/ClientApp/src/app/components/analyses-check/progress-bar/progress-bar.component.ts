6d74ef6ae55d65a6d1b275bf31748a04cb01eeba
Alena-Levina <alena.levina@softensity.com>
2022-06-08 00:31:27 +0200

    Progress bar: implemented full range dynamic coloring

--- MedExpert.Web/ClientApp/src/app/components/analyses-check/progress-bar/progress-bar.component.ts
+++ MedExpert.Web/ClientApp/src/app/components/analyses-check/progress-bar/progress-bar.component.ts
@@ -23,16 +23,21 @@ export class ProgressBarComponent implements OnInit {
     this.shouldSetWidth = true;
   }
 
-  get isGood(): boolean {
-    return this.percentage < 33.33
-  }
+  get backgroundColor(): string {
+    // returns background color for respective percentage.
 
-  get isNormal(): boolean {
-    return this.percentage >= 33.33 && this.percentage < 66.66;
-  }
-
-  get isBad(): boolean {
-    return this.percentage >= 66.66
+    // 0% color is green: #9ae98f = rgb(154, 233, 143) = hsl(113, 67%, 73%)
+    // 100% color is red: #e98f8f = rgb(233, 143, 143) = hsl(0, 67%, 73%)
+    // saturation and lightness doesn't depend on percentage
+    const saturation = 67, lightness = 73;
+    // hue does depend on percentage
+    const minPercentageHue = 113; // green (for 0% percentage)
+    const maxPercentageHue = 0; // red (for 100% percentage)
+    const hueRange = minPercentageHue - maxPercentageHue;
+    // the more percentage is, smaller the hue value
+    const hue = minPercentageHue - Math.round(hueRange * this.percentage / 100);
+    // return background color in HSL format;
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
   }
 
 }
